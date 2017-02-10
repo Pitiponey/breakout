54a96ae350689c09c47186f4b7b14e4a6e81005e
Pitiponey <pierre.pellegrini@divtec.ch>
2017-02-10 15:52:18 +0100

    Add the racket control

--- Racket.py
+++ Racket.py
@@ -1,8 +1,20 @@
 import cv2
-from Config import *
 
 class Racket():
-    def __init__(self, width, height, img):
+    def __init__(self, pos_x, pos_y, width, height, img):
+        self.pos_x  = pos_x
+        self.pos_y  = pos_y
         self.width  = width
         self.height = height
         self.img    = img
+
+    def changePositions(self, x, y):
+        self.pos_x = x
+        self.pos_y = y
+
+    def draw(self):
+        cv2.rectangle(self.img, (self.pos_x - self.width, self.pos_y - self.height), (self.pos_x + self.width, self.pos_y + self.height),
+                      (0, 0, 255), -1)
+        return self.img
+
+
