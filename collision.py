b1ccb88ace4070dc3edff37fcb4a8696a806a911
Pitiponey <pierre.pellegrini@divtec.ch>
2017-02-13 18:41:25 +0100

    Ajout des collisions avec les briques "BUUUUUUG"

--- collision.py
+++ collision.py
@@ -23,8 +23,11 @@ class Collision:
 
     @staticmethod
     def calcul_rebound_angle(ball_impact):
-        """if ball_impact < 0: ball_impact *= -1
+        if ball_impact < 0:
+            ball_impact *= -1
+
         inp = int(ball_impact / 9)
+
         if inp == 0:
             Config.ANGLE = 0
         elif inp == 1:
@@ -36,16 +39,15 @@ class Collision:
         elif inp == 4:
             Config.ANGLE = 90
         else:
-            Config.ANGLE = 80"""
-        Config.ANGLE = 0
+            Config.ANGLE = 80
 
     """
         Test si la balle et la raquette sont au même endroit à 10 px près
     """
     def collision_racket_ball(self):
         if self.racket_pos_x - self.racket_width / 2 < self.ball_pos_x + RADIUS / 2 < self.racket_pos_x + self.racket_width / 2 + RADIUS and \
-           self.racket_pos_y - self.racket_heigth / 2 - SPEED - SPEED / 2 < self.ball_pos_y < \
-           self.racket_pos_y - self.racket_heigth / 2 + SPEED - SPEED / 2:
+           self.racket_pos_y - self.racket_heigth / 2 - SPEED * 2 < self.ball_pos_y < \
+           self.racket_pos_y - self.racket_heigth / 2 + SPEED * 2:
 
             self.calcul_rebound_angle(self.racket_pos_x - self.ball_pos_x)
 
@@ -53,15 +55,41 @@ class Collision:
                 Config.UP = False
                 Config.DOWN = True
 
+
+    def collision_brick_ball(self, list_brick):
+        for pos_x, pos_y, number in list_brick:
+            #if self.ball_pos_x > pos_x and self.ball_pos_x < pos_x + BRICK_WIDTH and self.ball_pos_y > pos_y and self.ball_pos_y < pos_y + BRICK_HEIGHT:
+            if pos_x < self.ball_pos_x - (RADIUS * 2) < pos_x + BRICK_WIDTH and pos_y < self.ball_pos_y - \
+                    (RADIUS * 2) < pos_y + BRICK_HEIGHT:
+
+                del list_brick[int(number) - 1]
+
+                if Config.UP:
+                    Config.UP = False
+                    Config.DOWN = True
+
+                if Config.DOWN:
+                    Config.UP = True
+                    Config.DOWN = False
+
+                if Config.LEFT:
+                    Config.RIGHT = True
+                    Config.LEFT = False
+
+                if Config.RIGHT:
+                    Config.RIGHT = False
+                    Config.LEFT = True
+
     """
         Récupère les valeurs actuelles avant de tester si il y a une collision ou non
     """
-    def update(self, ball_pos_x, ball_pos_y, racket_pos_x, racket_pos_y):
+    def update(self, ball_pos_x, ball_pos_y, racket_pos_x, racket_pos_y, list_brick):
         self.ball_pos_x   = ball_pos_x
         self.ball_pos_y   = ball_pos_y
         self.racket_pos_x = racket_pos_x
         self.racket_pos_y = racket_pos_y
 
+        self.collision_brick_ball(list_brick)
         self.collision_racket_ball()
 
     """
